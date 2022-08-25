3e1e76c23c86ac819893f64ede46f7cef75fd7db
ashiikkhan <ashiksust06@gmail.com>
2022-08-25 19:15:08 +0600

    first commit

---
+++ chain.js
@@ -0,0 +1,17 @@
+const users = [{ id: 1, name: 'abul', job: 'doctor' }];
+// console.log(users[0].name);
+
+const data = {
+  count: 500,
+  data: [
+    { id: 1, name: 'john', job: 'designer' },
+    { id: 2, name: 'don', job: 'developer' },
+    { id: 3, name: 'peter', job: 'marketer' },
+  ],
+};
+// to get object property use dot(.)
+// to get array elements use index [i]
+// console.log(data);
+// console.log(data.data);
+// console.log(data.data[0]);
+// console.log(data.data[0].name);
