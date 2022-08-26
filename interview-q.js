f189d5788448331078dcf5682388befeab3ca9f1
ashiikkhan <ashiksust06@gmail.com>
2022-08-26 21:53:47 +0600

    interview questions|

---
+++ interview-q.js
@@ -0,0 +1,12 @@
+/**
+ * 1.What is the difference between map, foreach, filter?
+ * 2. What is the difference between filter and find?
+ * 3. What is the difference between for of and for in ?
+ * 4. How do you empty an array?
+ * 5. Difference between class and object?
+ * 6. What is the prototype chain? or How does inheritance work in javascript?
+ * 7. How does destruction do in es6?
+ * 8. Is optional chaining is same as ternary operator?
+ * 9. What do you mean by dot notaion and bracket notarion? When to use?
+ *
+ */
